b4d681eeb76295c6ecdb3372c3b1793ad56a877f
Alanvnorcott <anorcott@drew.edu>
2024-06-19 01:48:13 -0400

    Need projects and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
-import styles from './App.module.css'
-import { Navbar } from './components/Navbar/Navbar'
-import { Hero } from './components/Hero/Hero'
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-  return ( <div className={styles.App}>
-    <Navbar />
-    <Hero />
-  </div>
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+
+    </div>
   );
 }
-export default App
+
+export default App;
+
